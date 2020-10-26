28092fb0ba08460e86de451bfd8401bde9b8b449
EuanB26 <ewiebullmar1@gmail.com>
2020-10-26 17:10:20 +0000

    Creating a database locallyu

--- config.py
+++ config.py
@@ -1,4 +1,12 @@
 import os
+from dotenv import load_dotenv
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, '.env'))
 
 class Config(object):
     SECRET_KEY = os.getenv("SECRET_KEY")
+    SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL") or \
+    "sqlite:///" + os.path.join(basedir, "app.db")
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
--- project/__init__.py
+++ project/__init__.py
@@ -1,7 +1,11 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from config import Config
 
 project = Flask(__name__)
 project.config.from_object(Config)
+db = SQLAlchemy(project)
+migrate = Migrate(project, db)
 
-from project import routes
+from project import routes, models
