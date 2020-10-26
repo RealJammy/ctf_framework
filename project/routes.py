13b16a57526b83acc38b866db6195199f298aff9
EuanB26 <ewiebullmar1@gmail.com>
2020-10-26 16:34:32 +0000

    Fixed spelling mistakes

--- project/routes.py
+++ project/routes.py
@@ -1,6 +1,6 @@
 from project import project
 from project.forms import LoginForm
-from flask import render_template, flash, redirect
+from flask import render_template, flash, redirect, url_for
 
 @project.route("/")
 @project.route("/index")
@@ -12,7 +12,7 @@ def index():
 @project.route("/login", methods=["GET", "POST"])
 def login():
     form = LoginForm()
-    if form.vaildate_on_submit():
+    if form.validate_on_submit():
         flash(f"Login requested or user {form.username.data}, remember_me={form.remember_me.data}")
         return redirect(url_for("index"))
     return render_template("login.html", title="Sign in", form=form)
