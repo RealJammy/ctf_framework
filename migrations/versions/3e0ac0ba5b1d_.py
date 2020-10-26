d759d0ab044fcf5d309619a0c163fb5c303903f3
EuanB26 <ewiebullmar1@gmail.com>
2020-10-26 20:03:25 +0000

    Added user registration!

--- migrations/versions/3e0ac0ba5b1d_.py
+++ migrations/versions/3e0ac0ba5b1d_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: 6ac756277517
+Revision ID: 3e0ac0ba5b1d
 Revises: 
-Create Date: 2020-10-26 18:25:50.209316
+Create Date: 2020-10-26 19:42:14.529008
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '6ac756277517'
+revision = '3e0ac0ba5b1d'
 down_revision = None
 branch_labels = None
 depends_on = None
@@ -22,6 +22,7 @@ def upgrade():
     sa.Column('id', sa.Integer(), nullable=False),
     sa.Column('username', sa.String(length=64), nullable=True),
     sa.Column('email', sa.String(length=64), nullable=True),
+    sa.Column('password_hash', sa.String(length=128), nullable=True),
     sa.PrimaryKeyConstraint('id')
     )
     op.create_index(op.f('ix_user_email'), 'user', ['email'], unique=True)
--- project/routes.py
+++ project/routes.py
@@ -1,5 +1,5 @@
-from project import project, login
-from project.forms import LoginForm
+from project import project, login, db
+from project.forms import LoginForm, RegistrationForm
 from project.models import User
 from flask import render_template, flash, redirect, url_for, request
 from flask_login import current_user, login_user, logout_user, login_required
@@ -30,11 +30,21 @@ def login():
         return redirect(next_page)
     return render_template("login.html", title="Sign in", form=form)
 
-@login.user_loader
-def load_user(id):
-    return User.query.get(int(id))
-
 @project.route("/logout")
 def logout():
     logout_user()
     return redirect(url_for("index"))
+
+@project.route("/register", methods=["GET", "POST"])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for("index"))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash("Congratulations, you are now a registered user!")
+        return redirect(url_for("login"))
+    return render_template("register.html", title="Register", form=form)
