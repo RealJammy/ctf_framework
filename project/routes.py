85f31e44eaa76dadc673949d5f07c3f1b50f4c15
EuanB26 <ewiebullmar1@gmail.com>
2020-10-26 13:02:15 +0000

    Creating index.html and a template

--- project/routes.py
+++ project/routes.py
@@ -1,6 +1,9 @@
 from project import project
+from flask import render_template
 
 @project.route("/")
 @project.route("/index")
 def index():
-    return "Hello, World!"
+    user = {"username": "John"}
+    posts = [{"author": {"username": "Susan"}, "body": "Testing 1 2 3"}]
+    return render_template("index.html", title="Home", user=user, posts=posts)
