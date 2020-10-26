e044cd21a64af7fb67fc19dc3bcaccbc9fa7d49c
EuanB26 <ewiebullmar1@gmail.com>
2020-10-26 16:30:54 +0000

    Receiving data + errors in code

--- project/routes.py
+++ project/routes.py
@@ -1,6 +1,6 @@
 from project import project
 from project.forms import LoginForm
-from flask import render_template
+from flask import render_template, flash, redirect
 
 @project.route("/")
 @project.route("/index")
@@ -9,7 +9,10 @@ def index():
     posts = [{"author": {"username": "Susan"}, "body": "Testing 1 2 3"}]
     return render_template("index.html", title="Home", user=user, posts=posts)
 
-@project.route("/login")
+@project.route("/login", methods=["GET", "POST"])
 def login():
     form = LoginForm()
+    if form.vaildate_on_submit():
+        flash(f"Login requested or user {form.username.data}, remember_me={form.remember_me.data}")
+        return redirect(url_for("index"))
     return render_template("login.html", title="Sign in", form=form)
