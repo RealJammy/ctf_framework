167d9df774a8a972b3d777479dadb04529b6aef4
EuanB26 <ewiebullmar1@gmail.com>
2020-10-26 13:33:33 +0000

    Adding login page

--- project/routes.py
+++ project/routes.py
@@ -1,4 +1,5 @@
 from project import project
+from project.forms import LoginForm
 from flask import render_template
 
 @project.route("/")
@@ -7,3 +8,8 @@ def index():
     user = {"username": "John"}
     posts = [{"author": {"username": "Susan"}, "body": "Testing 1 2 3"}]
     return render_template("index.html", title="Home", user=user, posts=posts)
+
+@project.route("/login")
+def login():
+    form = LoginForm()
+    return render_template("login.html", title="Sign in", form=form)
